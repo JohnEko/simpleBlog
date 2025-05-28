8c238a4fc9517f6e262560416333e9cc5a58f6f0
JohnEko <zino@DESKTOP-QOOLFO0.localdomain>
2025-05-28 19:33:23 +0100

    login and register created

---
+++ client/my-app/app/login/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Router, { useRouter } from "next/router"
+import React, {useState, useEffect} from "react"
+import "./login.css"
+
+
+const Login = () => {
+  // const router = useRouter()
+  return (
+          <div>
+            
+              <form className="login" action="">
+                <h1>Login</h1>
+                <input type="text" placeholder="username" />
+                <input type="text" placeholder="password" />
+                <button>Login</button>
+
+              </form>
+          </div>
+    
+  )
+}
+export default Login
---
+++ client/my-app/app/register/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Router, { useRouter } from "next/router"
+import React, {useState, useEffect} from "react"
+import "./register.css"
+
+const Register = () => {
+  // const router = useRouter()
+  return (
+          <div>
+            
+              <form className="register" action="">
+                <h1>Login</h1>
+                <input type="text" placeholder="username" />
+                <input type="text" placeholder="password" />
+                <button>Register</button>
+
+              </form>
+          </div>
+    
+  )
+}
+export default Register
