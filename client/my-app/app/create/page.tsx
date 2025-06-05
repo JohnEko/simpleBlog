36042434cb03bc8ca7eaae8a85a8b4f66b2988a1
JohnEko <zino@DESKTOP-QOOLFO0.localdomain>
2025-06-05 20:32:18 +0100

    create Post form

--- client/my-app/app/create/page.tsx
+++ client/my-app/app/create/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+'use client'
+import ReactQuill from 'react-quill-new'
+import 'react-quill-new/dist/quill.snow.css'
+import dynamic from 'next/dynamic';
+import "./createPost.css"
 
-export default function CreatePost() {
+//const ReactQuill = dynamic(import("react-quill-new"),{ ssr: false });
+const CreatePost = () =>{
+
   return (
-    <div>CreatePost</div>
+    // <h2>CreatePost</h2>
+      <form>
+          <input type="title"  placeholder='{Title}'/>
+          <input type="summary"  placeholder='{Summary}'/>
+          <input type="file" />
+          <ReactQuill />
+      </form>
   )
 }
+
+export default CreatePost
--- client/my-app/app/logout/page.tsx
+++ client/my-app/app/logout/page.tsx
@@ -2,14 +2,17 @@
 
 import React, { useContext } from 'react'
 import { UserContext } from '../component/navbar/UserContext';
+import { userInfo } from 'node:os';
 
-const logoutPage = () => {
-    const {userInfo, setUserInfo} : any  = useContext(UserContext);
+const logoutPage =  () => {
+    const {userInfo, setUserInfo} :any  = useContext(UserContext);
+    
   
-    fetch('http://localhost:4000/logout', {
-        credentials: 'include', 
+    const response =  fetch('http://localhost:4000/logout', {
         method: 'POST'
-    })
+    }); if (response == userInfo){
+            setUserInfo(userInfo);
+    }
     //setUserInfo(null);
 
   return (
